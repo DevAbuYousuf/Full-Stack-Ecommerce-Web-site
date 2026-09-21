06f94127a5939d2610c037eb37931efdc4110880
YousufDev <mdyousuf3.ac@gmail.com>
2026-09-21 23:30:24 +0600

    add-logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,31 @@
-
+import Logo from '../../Assets/img/logo.jpg';
+import { Link } from 'react-router-dom';
 const Header =()=>{
     return(
         <>
-        <div className="header">
-            <div className="Top-Strip">
+        <div className="headerwraper">
+            <div className="Top-Strip bg-purple">
                 <div className="Container">
                     <p className="mb-0 mt-0 text-center">Free shipping on all orders over $50</p>
                 </div>
             </div>
         </div>
+        <div className="header">
+            <div className="container">
+                <div className="row">
+                    <div className="logowrapper col-sm-2">
+                            <Link to={'/'}>
+                                <img src={Logo} alt='Logo'/>
+                            </Link>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
         </>
     )
 }
